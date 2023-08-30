28d5b8075db189bb452ef733ad8da421cb8c9188
Fatih Parmaksız <fatihparm6@gmail.com>
2023-08-31 02:58:06 +0300

    modified

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def burc(ctx, sign):
             embed = format_horoscope_embed(sign_dictionary[sign])
             await ctx.send(embed=embed)
     else:
-        await ctx.send("Geçerli bir burç ismi giriniz. (Türkçe karakter kullanmayınız)")
+        await ctx.send("Geçerli bir burç ismi giriniz.")
 
 
 @tasks.loop(hours=24)
